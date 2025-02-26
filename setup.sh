f26739028a36cc51171d0d861bc0556f83aede75
Pau Vilella <pau.vilella@apple.com>
2025-02-26 12:12:07 +0100

    don't have github username and repo as an input in the script

--- setup.sh
+++ setup.sh
@@ -13,10 +13,8 @@ kind create cluster --config kind.yaml
 # Install flux
 echo "Installing flux..."
 export GITHUB_TOKEN=$(GH_HOST=github.com gh auth token)
-read -p "Enter GitHub username [default: pauvilella]: " GITHUB_USER
-read -p "Enter GitHub repo [default: crossplane-poc]: " GITHUB_REPO
-GITHUB_USER=${GITHUB_USER:-"pauvilella"}
-GITHUB_REPO=${GITHUB_REPO:-"crossplane-poc"}
+GITHUB_USER="pauvilella"
+GITHUB_REPO="crossplane-poc"
 flux bootstrap github \
   --branch=main \
   --context=kind-kind \
